33d04704964f5caae36191c8502d71064578cd34
samuele301197 <parisisamu32@gmail.com>
2025-04-17 12:15:08 +0200

    adjust:risultati-bonus

--- js-arrays-bonus/script.js
+++ js-arrays-bonus/script.js
@@ -10,10 +10,29 @@ const teachers = [
   
   // 1. Inverti l'ordine degli insegnanti nell'array teachers
   // e salva il risultato nella variabile reversedTeachers
-  const reversedTeachers = null;
+  // METODO SEMPLIFICATO
+  const reversedTeachers = teachers.reverse();
+  console.log(teachers)
+
+// METODO PRIMARIO
+  for (let i = teachers.length - 1; i >= 0; i--) {
+    const curElem = teachers[i];
+    reversedTeachers.push(curElem);
+  }
+  console.log(reversedTeachers)
   
   // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
   // con un nome di lunghezza maggiore o uguale a 5 caratteri
   const longNames = null;
+
+
+  for (let i = 0; i < teachers.length; i++) {
+    const curElem = teachers[i]
+    if (curElem.length >= 5) {
+      longNames.push(curElem);
+    }
+  }
+  console.log(longNames);
+  
   
   // 3. Rimuovi 'Ed' dall'array teachers
